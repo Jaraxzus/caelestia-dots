8149bd7a782a0a7873b9f8af15efe9db0426a03b
jaraxzus <alex099024@gmail.com>
2026-01-11 11:56:04 +0300

    update config

--- fish/config.fish
+++ fish/config.fish
@@ -1,10 +1,11 @@
 if status is-interactive
     # Starship custom prompt
-    starship init fish | source
+    # starship init fish | source
 
     # Direnv + Zoxide
     command -v direnv &> /dev/null && direnv hook fish | source
     command -v zoxide &> /dev/null && zoxide init fish --cmd cd | source
+    
 
     # Better ls
     alias ls='eza --icons --group-directories-first -1'
@@ -35,8 +36,12 @@ if status is-interactive
     # Custom colours
     cat ~/.local/state/caelestia/sequences.txt 2> /dev/null
 
+
     # For jumping between prompts in foot terminal
     function mark_prompt_start --on-event fish_prompt
         echo -en "\e]133;A\e\\"
     end
 end
+set -g fish_color_error red --bold
+set -gx EDITOR nvim
+set -gx VISUAL nvim
